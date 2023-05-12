82eb6c135bf54d54e96f9c8c19bd9748cf792ae7
Bert Vercaemer <bvercaem@fu-r5-p5.s19.be>
2023-05-12 14:47:37 +0200

    happy little norminette

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 12:16:24 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/05/12 14:25:51 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/05/12 14:45:52 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static char	*ft_newbuf_freeline(char *buffer, char *line)
 	size_t	i;
 	size_t	end;
 
-//printf("##############################\nnewbuf input:\nbuffer = \n|%s|\nline = \n|%s|\n\n", buffer, line);
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
@@ -35,7 +34,6 @@ static char	*ft_newbuf_freeline(char *buffer, char *line)
 		new[end - i] = buffer[end];
 	free(line);
 	free(buffer);
-//printf("newbuf output = \n|%s|\n##############################\n\n\n", new);
 	return (new);
 }
 
@@ -55,25 +53,17 @@ static char	*ft_append_res(char *line, char **buffer, ssize_t i)
 	char	*res;
 
 	ft_conform_input(&i, buffer);
-//printf("\n\n\t%%%050%\nappend_res input:\ni = %zd\nline = \n|%s|\nbuffer = \n|%s|\n\n", i, line, *buffer);
 	len = 0;
 	while (line[len])
 		len++;
-//printf("\n1-coucou i = %zd /// len = %zu\n", i, len);
-//printf("\nline = %p\nbuffer = %p\n&buffer = %p\n\n", line, *buffer, buffer);
 	res = malloc(len + i + 1);
-//printf("\nres = %p\nline = %p\nbuffer = %p\n&buffer = %p\n\n", res, line, *buffer, buffer);
 	if (!res)
 		return (ft_null(line, buffer));
-//write(1, "\n2-coucou\n", 10);
 	res[len + i] = 0;
 	while (i--)
 		res[len + i] = (*buffer)[i];
-//write(1, "\n3-coucou\n", 10);
 	while (len--)
 		res[len] = line[len];
-//printf("after line cpy = |%s|\n", res);
-//printf("append_res: output = \n|%s|\n\t%%%050%\n\n\n", res);
 	*buffer = ft_newbuf_freeline(*buffer, line);
 	return (res);
 }
@@ -99,6 +89,5 @@ char	*get_next_line(int fd)
 		check = read(fd, buffer, BUFFER_SIZE);
 		buffer[check] = 0;
 	}
-//printf("\n--------------- THAT WAS ALL FOLKS ---------------\n");
 	return (ft_null(line, &buffer));
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,16 +6,13 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:25:16 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/05/12 13:18:29 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/05/12 14:46:00 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-		//AAAAAAAAAAAAAAAAAAAAAAAAAH
-		#include <stdio.h>
-		#include <unistd.h>
-		//BBBBBBBBBBBBBBBBBBBBBBBBBH
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100
 # endif
