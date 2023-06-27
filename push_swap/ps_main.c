2702eae2c4326421354c0b0daac98730a2ed45fc
Bert Vercaemer <bvercaem@fu-r8-p5.s19.be>
2023-06-27 15:06:38 +0200

    should be norm compliant now

--- push_swap/ps_main.c
+++ push_swap/ps_main.c
@@ -6,41 +6,44 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 14:06:23 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/06/24 21:02:13 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/06/27 15:03:51 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	test_print(t_stack *a, char c)
-{
-/* THIS IS A TEST FUNCTION TO PRINT MY CONTAINERS */
-	t_dlilist	*list;
+// void	test_print(t_stack *a, char c)
+// {
+// /* THIS IS A TEST FUNCTION TO PRINT MY CONTAINERS */
+// 	t_dlilist	*list;
 
-	list = a->start;
-	if (a->chunks)
-		ft_printf("%c: s[%i]c[%i] {", c, a->size, a->chunks->size);
-	else
-		ft_printf("%c: s[%i]c[N/A] {", c, a->size);
-	while (list && list->next)
-	{
-		ft_printf("%i, ", list->nb);
-		list = list->next;
-	}
-	if (list)
-		ft_printf("%i", list->nb);
-	if (a->chunks)
-		ft_printf("}  s(%i, %i)c(%i, %i)\n", a->min, a->max, a->chunks->min, a->chunks->max);
-	else
-		ft_printf("}  s(%i, %i)c(N/A, N/A)\n", a->min, a->max);
-	if (a->size && (a->end->nb != list->nb))
-	{
-		ft_printf("!!!LIST END IS NOT CORRECT!!! end = %i\n", a->end->nb);
-	}
-}
+// 	list = a->start;
+// 	if (a->chunks)
+// 		ft_printf("%c: s[%i]c[%i] {", c, a->size, a->chunks->size);
+// 	else
+// 		ft_printf("%c: s[%i]c[N/A] {", c, a->size);
+// 	while (list && list->next)
+// 	{
+// 		ft_printf("%i, ", list->nb);
+// 		list = list->next;
+// 	}
+// 	if (list)
+// 		ft_printf("%i", list->nb);
+// 	if (a->chunks)
+// 		ft_printf("}  s(%i, %i)c(%i, %i)\n"
+//		, a->min, a->max, a->chunks->min, a->chunks->max);
+// 	else
+// 		ft_printf("}  s(%i, %i)c(N/A, N/A)\n", a->min, a->max);
+// 	if (a->size && (a->end->nb != list->nb))
+// 	{
+// 		ft_printf("!!!LIST END IS NOT CORRECT!!! end = %i\n", a->end->nb);
+// 	}
+// }
 
+// maybe add conditions in operation fts:
+//	that check the last element of the print list for a more efficient list
+//	(instead of doing it afterwards with just the list).
 int	main(int ac, char *av[])
-// maybe add conditions in operation fts that check the last element of the print list for a more efficient list (instead of doing it afterwards).
 {
 	t_stack	a;
 	t_stack	b;
@@ -58,37 +61,8 @@ int	main(int ac, char *av[])
 	ps_print_compression(&a);
 	ps_print_ops(&a);
 	ps_print_clear(&a);
-
-/* THIS IS THE START OF MY LITTLE TEST AREA */
-
-if (b.size || b.start || b.end)
-{
-	ft_printf("\n[!!!] [STACK 'b' ISN'T EMPTY] [!!!]\n");
-	test_print(&b, 'b');
-}
-if (!ps_issorted(&a, 1, 0))
-{
-	ft_printf("\n[!!!] [STACK 'a' ISN'T SORTED] [!!!]\n");
-	test_print(&a, 'a');
-}
-
-// ps_push(&a, &b, 'b');
-// test_print(&a, 'a');
-// test_print(&b, 'b');
-// ps_push(&b, &a, 'a');
-// test_print(&a, 'a');
-// test_print(&b, 'b');
-// ps_push(&b, &a, 'a');
-// test_print(&a, 'a');
-// test_print(&b, 'b');
-
-/* THIS IS THE END OF MY LITTLE TEST AREA */
-
 	while (ps_del_back(&a, 1) || ps_del_back(&b, 1))
 		(void)a;
 	while (ps_del_chunk(&a) || ps_del_chunk(&b))
 		(void)a;
-
-// ft_printf("\n\t--------------------------------\n\n");
-// system("leaks push_swap");
 }
