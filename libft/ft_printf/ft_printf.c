be89aadcc9ee9162d807f9c7e04f0a2c9b186370
Bert Vercaemer <bvercaem@fu-r8-p8.s19.be>
2023-07-07 13:13:45 +0200

    added custom fd functionality to ft_printf

--- libft/ft_printf/ft_printf.c
+++ libft/ft_printf/ft_printf.c
@@ -6,11 +6,26 @@
 /*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 13:17:15 by bvercaem          #+#    #+#             */
-/*   Updated: 2023/05/11 17:00:00 by bvercaem         ###   ########.fr       */
+/*   Updated: 2023/07/07 13:03:52 by bvercaem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_conv_fd(const char **pform, va_list *pva)
+{
+	int	fd;
+
+	if (*pform[0] != '%')
+		return (STDOUT_FILENO);
+	if (*pform[1] != 'f')
+		return (STDOUT_FILENO);
+	if (*pform[2] != 'd')
+		return (STDOUT_FILENO);
+	fd = va_arg(*pva, int);
+	*pform += 3;
+	return (fd);
+}
 
 static short	ft_check_spec(const char *format, const char *set)
 {
@@ -52,38 +67,42 @@ static char	*ft_cut_format(const char **pform)
 }
 
 // returns -1 on failure, else returns 0
-static short	ft_output(char *to_print, int *mlen, int *rlen)
+static short	ft_output(t_to_print to_print, int *mlen, int *rlen)
 {
 	int	check;
 
-	if (!to_print)
+	if (!to_print.str)
 		return (-1);
-	check = write(1, to_print, *mlen);
-	free (to_print);
+	check = write(to_print.fd, to_print.str, *mlen);
+	free (to_print.str);
 	if (check == -1)
 		return (ft_error_minone("write", "output", NULL));
 	*rlen += check;
 	return (0);
 }
 
+// if the format starts with '%fd' the provided fd is used, else the stdout.
 int	ft_printf(const char *format, ...)
 {
-	va_list	va;
-	int		rlen;
-	char	*sform;
-	char	*to_print;
-	int		mlen;
+	va_list		va;
+	int			rlen;
+	char		*sform;
+	t_to_print	to_print;
+	int			mlen;
 
 	if (!format)
-		return (-1);
+		return (ft_error_minone("a NULL format", "ft_printf", NULL));
 	rlen = 0;
 	va_start(va, format);
+	to_print.fd = ft_conv_fd(&format, &va);
+	if (to_print.fd < 1 || to_print.fd > FOPEN_MAX)
+		return (ft_error_minone("the fd", "ft_printf", &va));
 	while (*format)
 	{
 		sform = ft_cut_format(&format);
 		if (!sform)
 			return (ft_error_minone(NULL, NULL, &va));
-		to_print = ft_conv_hub(sform, &mlen, &va);
+		to_print.str = ft_conv_hub(sform, &mlen, &va);
 		free(sform);
 		if (ft_output(to_print, &mlen, &rlen))
 			return (ft_error_minone(NULL, NULL, &va));
