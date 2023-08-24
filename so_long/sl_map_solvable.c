0fdd077d72fabba7a6e3acc765e1c4828cd8299f
Bert Vercaemer <bvercaem@fu-r6-p5.s19.be>
2023-08-24 19:29:10 +0200

    so_long: added mostly empty file for checking if the map is solvable

---
+++ so_long/sl_map_solvable.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sl_map_solvable.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bvercaem <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 15:16:23 by bvercaem          #+#    #+#             */
+/*   Updated: 2023/08/24 19:28:04 by bvercaem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static t_sl_tile	sl_solv_start(t_sl_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 1;
+	while()
+}
+
+int	sl_map_solvable(t_sl_data *data, t_sl_map *checks)
+{
+	int	col;
+	int	exi;
+
+	col = data->clbls;
+	exi = 1;
+}
